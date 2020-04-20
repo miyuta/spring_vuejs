98f46f564d18d5db9cd7c98caba5e2bab4d714eb
miyuta <yuemiyuta@gmail.com>
2020-04-21 04:23:48 +0900

    Connect Spring, Vue

---
+++ gradleVue/src/main/java/com/example/gradle01/GradleVueApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradle01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleVueApplication.class, args);
+	}
+
+}
---
+++ gradleVue/src/test/java/com/example/gradle01/GradleVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradle01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleVueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
